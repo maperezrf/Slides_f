4e096453c17c3a754f7a44dc4f24698f0a0603fb
Alejandro Parra <pa.parra12@uniandes.edu.co>
2022-03-15 16:30:00 -0500

    todos iniciales

---
+++ f4_classifier.py
@@ -0,0 +1,21 @@
+# Libraries
+
+# TODO importar librerias 
+
+# Variables 
+
+# TODO cargar archivos
+
+# Trasform 
+
+# TODO transformar variables 
+
+# Methods 
+
+# TODO un método para las reglas de tiendas y otro para CD 
+
+# Output 
+
+# TODO imprimir monto y cantidad de f4 según estado, f4 clasificados, f4 sin clasificar
+
+# TODO guardar f4 en registrado, f4 no clasificados y consolidado nuevo
---
+++ fig_slides.py
@@ -0,0 +1,20 @@
+# Libraries
+
+# TODO importar librerias 
+
+# Variables 
+
+# TODO cargar archivos f3, f4, f11
+
+# Trasform 
+
+# TODO transformar variables 
+
+# Methods 
+
+# TODO crear método por f y por imagen 
+# Nombralos fx_slide() y fig_fx_xxx()
+
+# Output 
+
+# TODO guardar figuras en una carpeta para cada corte 
